ea2ab5905d6cb4205fb9de7cec204b87b60b7bab
Maciejoss <maciejkordyaczny@gmail.com>
2024-11-12 17:12:42 +0100

    add regexp validation to user properties

--- Java/spring-thymeleaf-crud-example/src/main/java/com/example/thymeleaf/dto/CreateStudentDTO.java
+++ Java/spring-thymeleaf-crud-example/src/main/java/com/example/thymeleaf/dto/CreateStudentDTO.java
@@ -7,6 +7,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import java.time.LocalDate;
 
 @Getter
@@ -14,6 +15,7 @@ import java.time.LocalDate;
 public class CreateStudentDTO {
 
     @NotEmpty(message = "{NotEmpty.name}")
+    @Pattern(regexp = "^[\\p{L} .'-]+$", message = "{Pattern.name}")
     private String name;
 
     @Email
@@ -25,23 +27,30 @@ public class CreateStudentDTO {
     private LocalDate birthday;
 
     @NotEmpty(message = "{NotEmpty.zipCode}")
+    @Pattern(regexp = "^[0-9-]+$", message = "{Pattern.zipCode}")
     private String zipCode;
 
     @NotEmpty(message = "{NotEmpty.street}")
+    @Pattern(regexp = "^[\\p{L}0-9 .'-]+$", message = "{Pattern.street}")
     private String street;
 
     @NotEmpty(message = "{NotEmpty.number}")
+    @Pattern(regexp = "^[\\p{L}0-9]+$", message = "{Pattern.number}")
     private String number;
 
+    @Pattern(regexp = "^[\\p{L}0-9 .'-]*$", message = "{Pattern.complement}")
     private String complement;
 
     @NotEmpty(message = "{NotEmpty.district}")
+    @Pattern(regexp = "^[\\p{L} .'-]+$", message = "{Pattern.district}")
     private String district;
 
     @NotEmpty(message = "{NotEmpty.city}")
+    @Pattern(regexp = "^[\\p{L} .'-]+$", message = "{Pattern.city}")
     private String city;
 
     @NotEmpty(message = "{NotEmpty.state}")
+    @Pattern(regexp = "^[\\p{L} .'-]+$", message = "{Pattern.state}")
     private String state;
 
 }
